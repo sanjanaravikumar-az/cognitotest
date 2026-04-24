0bc94301aa7bbd12ba64ae4763aa70be98f4875b
sanjanaravikumar-az <sanjana.ravikumar.az@gmail.com>
2026-04-24 15:52:48 -0400

    fix: npm install

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
-import amplifyconfig from './amplifyconfiguration.json';
+import amplifyconfig from '../amplify_outputs.json';
 import './index.css'
 import App from './App.tsx'
 
